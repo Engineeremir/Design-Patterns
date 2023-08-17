31938b418656cfd9107afeaf1740e7c7079e8c5c
emirhan.kalem320 <emirhan.kalem320@gmail.com>
2023-08-17 16:25:53 +0300

    Singleton pattern

---
+++ DesignPatterns.Singleton/CountryProvider.cs
@@ -0,0 +1,28 @@
+﻿using DesignPatterns.Singleton.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class CountryProvider
+    {
+        private static CountryProvider instance;
+        public static CountryProvider Instance => instance ?? (instance = new CountryProvider());
+        private new List<Country> Countries { get; set; }
+        private CountryProvider()
+        {
+            Task.Delay(2000).GetAwaiter().GetResult();
+            Countries = new List<Country>()
+                {
+
+                new Country(){Name="Turkey"},
+                new Country(){Name="Germany"}
+
+                };
+        }
+        public async Task<List<Country>> GetCountries() => Countries;
+    }
+}
---
+++ DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,7 @@
+﻿using DesignPatterns.Singleton;
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries = await CountryProvider.Instance.GetCountries();
+
+foreach (var country in countries)
+    Console.WriteLine(country.Name);
